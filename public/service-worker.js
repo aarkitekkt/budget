0de5bdec8fd78e2223f2f87be93b14feb1646e6b
aarkitekkt <johnson.brycej@gmail.com>
2020-04-04 15:28:02 -0600

    remove webpack

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,10 +2,8 @@ const FILES_TO_CACHE = [
     "/",
     "/index.html",
     "/styles.css",
-    "/manifest.webmanifest",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
-    "/dist/bundle.js",
     "/db.js"
 ];
 
